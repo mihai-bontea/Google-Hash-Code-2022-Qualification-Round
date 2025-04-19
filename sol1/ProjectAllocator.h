93cc956dc0edee8b819b81cf4a34bc3beb2aee0d
mihai-bontea <mihaibontea@protonmail.com>
2025-04-19 23:09:24 +0300

    binghe binghe

--- sol1/ProjectAllocator.h
+++ sol1/ProjectAllocator.h
@@ -1,103 +1,148 @@
 #pragma once
 
 #include <chrono>
+#include <algorithm>
+#include <random>
 
 #include "SimulationState.h"
+#include "BS_thread_pool.hpp"
 
 using namespace std::chrono;
+
+using WeightedAllocation = std::pair<int, std::vector<std::string>>;
 
 class ProjectAllocator
 {
 public:
-    static std::vector<int> find_allocation_for_project(SimulationState& simulation_state, const Project& project)
+    static WeightedAllocation find_alloc(const SimulationState& simulation_state, const std::vector<std::pair<std::string, int>>& skill_to_level)
     {
-        std::vector<int> current_solution_alloc, valid_solution_alloc;
-        std::map<int, bool> contributor_already_chosen;
+        std::random_device rd;
+        std::mt19937 g(rd());
+        auto skill_to_level_copy = skill_to_level;
+        std::shuffle(skill_to_level_copy.begin(), skill_to_level_copy.end(), g);
+
+        std::map<std::string, bool> contr_already_chosen, needs_mentorship;
+        std::map<std::string, std::string> role_to_contr, valid_solution;
+        int solution_points = 0;
 
         auto start = steady_clock::now();
         std::function<void(int)> find_allocation_backtracking = [&](int step){
-
             auto now = steady_clock::now();
+
+            // Solution found/timer expired, stop execution
             auto elapsed = duration_cast<seconds>(now - start);
-            if (!valid_solution_alloc.empty() || elapsed.count() >= 20)
+            if (!valid_solution.empty() || elapsed.count() >= 20)
                 return;
 
-            if (step == project.skill_to_level.size())
+            if (step == skill_to_level.size())
             {
-                int skill_id = 0;
-                bool solution_is_valid = true;
-                for (int contributor_id : current_solution_alloc)
+                bool is_solution_valid = true;
+                int learning_points = 0;
+                for (const auto& [curr_skill, level_req] : skill_to_level)
                 {
-                    auto [curr_skill, level_req] = project.skill_to_level[skill_id];
-                    if (simulation_state.contributors[contributor_id].skill_to_level[curr_skill] < level_req)
+                    const auto& contr_for_role = role_to_contr[curr_skill];
+                    const auto contr_per_levels = simulation_state.skill_to_contributors.find(curr_skill)->second;
+
+                    auto can_others_mentor = [&contr_per_levels, &role_to_contr, level_req](){
+                        for (const auto& [_, contr_name]: role_to_contr)
+                            for (int level = level_req; level <= 20; ++level)
+                                if (contr_per_levels[level].contains(contr_name))
+                                    return true;
+                        return false;
+                    };
+
+                    auto needs_mentorship = [&contr_per_levels, level_req](const std::string& contr_name){
+                        return contr_per_levels[level_req - 1].contains(contr_name);
+                    };
+
+                    // Needs mentorship
+                    if (needs_mentorship(contr_for_role))
                     {
-                        auto can_others_mentor = [&](){
-                             for (int colleague_id : current_solution_alloc)
-                                 if (colleague_id != contributor_id)
-                                 {
-                                     auto colleague_skill_level = simulation_state.contributors[colleague_id].skill_to_level[curr_skill];
-                                     if (colleague_skill_level >= level_req)
-                                         return true;
-                                 }
-                             return false;
-                        };
-                        if (!can_others_mentor())
+                        if (can_others_mentor())
                         {
-                            solution_is_valid = false;
+                            learning_points++;
+                        }
+                        else
+                        {
+                            is_solution_valid = false;
                             break;
                         }
                     }
-                    skill_id++;
                 }
-                if (solution_is_valid)
-                    valid_solution_alloc = current_solution_alloc;
+                if (is_solution_valid)
+                {
+                    solution_points = learning_points;
+                    valid_solution = role_to_contr;
+                }
             }
             else
             {
-                auto [curr_skill, level_req] = project.skill_to_level[step];
-
-                // Contributors who have this skill
-                for (int contributor_id : simulation_state.skill_to_contrib_id[curr_skill])
+                auto [curr_skill, level_req] = skill_to_level_copy[step];
+                // For skill between [level_req - 1, max)
+                for (int contr_level = level_req - 1; contr_level <= 20; ++contr_level)
                 {
-                    auto& contributor = simulation_state.contributors[contributor_id];
-                    const int contributor_skill = contributor.skill_to_level[curr_skill];
+                    const auto contr_per_levels = simulation_state.skill_to_contributors.find(curr_skill)->second;
+                    // Going over all contributors with skill == contr_level
+                    for (const auto &contr_name: contr_per_levels[contr_level])
+                    {
+                        int contr_available_at = (simulation_state.available_at.find(contr_name)->second);
+                        // Skip if busy at this day, or already chosen
+                        if (contr_already_chosen[contr_name] || contr_available_at > simulation_state.day)
+                            continue;
 
-                    // Too low skill, already chosen for this project, or busy with another project
-                    if (contributor_skill < level_req - 1 ||
-                    contributor_already_chosen[contributor_id] ||
-                    contributor.busy_until > simulation_state.day)
-                        continue;
+                        contr_already_chosen[contr_name] = true;
+                        role_to_contr[curr_skill] = contr_name;
 
-                    contributor_already_chosen[contributor_id] = true;
-                    current_solution_alloc.push_back(contributor_id);
+                        find_allocation_backtracking(step + 1);
 
-                    find_allocation_backtracking(step + 1);
-
-                    current_solution_alloc.pop_back();
-                    contributor_already_chosen[contributor_id] = false;
+                        contr_already_chosen[contr_name] = false;
+                    }
                 }
             }
         };
-
         find_allocation_backtracking(0);
-        return valid_solution_alloc;
+
+        // No solution found in a reasonable timeframe
+        if (valid_solution.empty())
+            return {0, {}};
+
+        // Formatting the solution
+        std::vector<std::string> solution_in_order;
+        for (const auto& [skill_name, _] : skill_to_level)
+        {
+            solution_in_order.push_back(valid_solution[skill_name]);
+        }
+        return {solution_points, solution_in_order};
+    }
+
+    static WeightedAllocation find_allocation_for_project(const SimulationState& simulation_state, const Project& project)
+    {
+        std::vector<std::pair<std::string, int>> skill_to_level;
+        for (const auto& [skill_name, level_req] : project.skill_to_level)
+            skill_to_level.emplace_back(skill_name, level_req);
+
+        BS::thread_pool pool(10);
+        std::vector<std::future<WeightedAllocation>> futures;
+        for (int th_index = 0; th_index < 10; ++th_index)
+        {
+            auto future_for_task = pool.submit_task([&]() { return find_alloc(simulation_state, skill_to_level); });
+            futures.push_back(std::move(future_for_task));
+        }
+
+        WeightedAllocation best_allocation = {0, {}};
+        for (auto& future : futures)
+        {
+            auto weighted_allocation = future.get();
+            const auto& [learning_points, allocation] = weighted_allocation;
+
+            if (!allocation.empty() && learning_points >= best_allocation.first)
+                best_allocation = weighted_allocation;
+        }
+        return best_allocation;
     }
 
     static void update_contributors(const Data& data, SimulationState& simulation_state, const ProjectAllocation& project_allocation)
     {
-        const auto& [project_id, contributor_ids] = project_allocation;
-        const auto& project = data.projects[project_id];
 
-        int skill_id = 0;
-        for (int contributor_id : contributor_ids)
-        {
-            auto& contributor = simulation_state.contributors[contributor_id];
-            contributor.busy_until = simulation_state.day + project.length_in_days;
-
-            auto [curr_skill, level_req] = project.skill_to_level[skill_id];
-            if (contributor.skill_to_level[curr_skill] < level_req)
-                contributor.skill_to_level[curr_skill] = level_req;
-            skill_id++;
-        }
     }
 };
